33813bc8c9cf3ee1f9ed64b04d7ba9d6f41d6d32
Saqlain Javed <saqlainish78@gmail.com>
2025-02-14 15:51:35 +0500

    completed

---
+++ text-to-speech/public/script.js
@@ -0,0 +1,30 @@
+const voiceSelect = document.querySelector("#voiceSelect");
+const playButton = document.querySelector("#playButton");
+const textInput = document.querySelector("textarea");
+
+// Load available voices
+let voices = [];
+function loadVoices() {
+  voices = speechSynthesis.getVoices();
+  voiceSelect.innerHTML = voices
+    .map(
+      (voice, index) =>
+        `<option value=${index}">${voice.name} (${voice.lang})</option>`
+    )
+    .join("");
+}
+
+// Trigger loading voices when they become available
+speechSynthesis.onvoiceschanged = loadVoices;
+loadVoices();
+
+// Play TTS
+
+playButton.addEventListener("click", () => {
+  const utterance = new SpeechSynthesisUtterance(textInput.value);
+  const selectedVoice = voices[voiceSelect.value];
+  if (selectedVoice) utterance.voice = selectedVoice;
+  speechSynthesis.speak(utterance);
+});
+
+console.log(textInput.value);
